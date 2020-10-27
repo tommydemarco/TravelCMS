47aa41df587b6b18baa86035e0096b1f19cd3762
tommydemarco <tommaso.demarco95@gmail.com>
2020-10-27 14:13:16 +0000

    Updated connection with private variables

--- models/connection.php
+++ models/connection.php
@@ -1,9 +1,15 @@
 <?php
 
+require 'private_variables.php';
+
 class Connection {
     
     static public function connect() {
-        $connection_link = new PDO('mysql:host=localhost;dbname=travel_cms', 'root', '');
+
+        $username = User::$username;
+        $password = User::$password;
+
+        $connection_link = new PDO('mysql:host=localhost;dbname=travel_cms', "$username", "$password");
 
         //functions not to get problems with the latin characters
         $connection_link -> exec("set names utf8");
